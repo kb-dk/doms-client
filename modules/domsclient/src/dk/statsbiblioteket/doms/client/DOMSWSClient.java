ca5cfeba02a209ff7227481676f2b733dac854dd
Thomas Skou Hansen <tsh@statsbiblioteket.dk>
2010-10-18 09:49:50 +0000

    Fixed the package statement.

--- modules/domsclient/src/dk/statsbiblioteket/doms/client/DOMSWSClient.java
+++ modules/domsclient/src/dk/statsbiblioteket/doms/client/DOMSWSClient.java
@@ -24,7 +24,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package dk.statsbiblioteket.doms.ingesters.radiotv;
+package dk.statsbiblioteket.doms.client;
 
 import java.io.ByteArrayInputStream;
 import java.net.URL;
