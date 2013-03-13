648ecf69c8c5c81a5360ce676fdb2cb05fcd10f1
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-03-13 15:37:32 +0100

    And now it builds

--- domsClient-common-interface/src/main/java/dk/statsbiblioteket/doms/client/objects/MissingObject.java
+++ domsClient-common-interface/src/main/java/dk/statsbiblioteket/doms/client/objects/MissingObject.java
@@ -2,6 +2,7 @@ package dk.statsbiblioteket.doms.client.objects;
 
 import dk.statsbiblioteket.doms.client.datastreams.Datastream;
 import dk.statsbiblioteket.doms.client.exceptions.ServerOperationFailed;
+import dk.statsbiblioteket.doms.client.links.LinkPattern;
 import dk.statsbiblioteket.doms.client.methods.Method;
 import dk.statsbiblioteket.doms.client.relations.LiteralRelation;
 import dk.statsbiblioteket.doms.client.relations.ObjectRelation;
@@ -143,6 +144,11 @@ public class MissingObject implements DigitalObject {
 
     @Override
     public Set<Method> getMethods() throws ServerOperationFailed {
+        throw new IllegalAccessError("Missing object");
+    }
+
+    @Override
+    public List<LinkPattern> getLinkPatterns() throws ServerOperationFailed {
         throw new IllegalAccessError("Missing object");
     }
 
