8b10551457ca54fc7904fa429c7a7c587e5639b8
Colin Rosenthal <csr@statsbiblioteket.dk>
2014-06-19 11:41:58 +0200

    Fixed up parse method to return a String instead of printing it.

--- domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/SdoUtils.java
+++ domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/SdoUtils.java
@@ -2,62 +2,69 @@ package dk.statsbiblioteket.doms.client.objects;
 
 import dk.statsbiblioteket.doms.client.sdo.SDOParsedXmlDocument;
 import dk.statsbiblioteket.doms.client.sdo.SDOParsedXmlElement;
+import dk.statsbiblioteket.util.Strings;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static org.junit.Assert.fail;
 
 /**
 * Created by csr on 19/06/14.
 */
 public class SdoUtils {
 
-    protected static void parseDoc(SDOParsedXmlDocument doc) {
-        System.out.println("'" + doc.getRootSDOParsedXmlElement().getLabel() + "'");
-        parseTree(doc.getRootSDOParsedXmlElement(), "");
+    protected static String parseDoc(SDOParsedXmlDocument doc) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("'" + doc.getRootSDOParsedXmlElement().getLabel() + "'\n");
+        parseTree(doc.getRootSDOParsedXmlElement(), "", stringBuilder);
+        return stringBuilder.toString();
     }
 
-    protected static void parseTree(SDOParsedXmlElement doc, String indryk) {
-
-
+    protected static void parseTree(SDOParsedXmlElement doc, String indryk, StringBuilder stringBuilder) {
         indryk = indryk + "   ";
         ArrayList<SDOParsedXmlElement> children = doc.getChildren();
         for (SDOParsedXmlElement child : children) {
             if (child.isLeaf()) {
-
-                System.out.print(indryk + "'" + child.getLabel() + "': '" + child.getValue() + "'");
+                stringBuilder.append(indryk + "'" + child.getLabel() + "': '" + child.getValue() + "'");
                 if (child.getProperty().isMany()) {
                     if (child.getAddable()) {
-                        System.out.print(" (+)");
+                        stringBuilder.append(" (+)");
                     }
                     if (child.getRemovable()) {
-                        System.out.print("(-)");
+                        stringBuilder.append("(-)");
                     }
                     if (child.hasNonEmptyDescendant()) {
-                        System.out.print("(d)");
+                        stringBuilder.append("(d)");
                     }
                 }
-                System.out.print("  type=" + child.getGuiTypeAsString());
-                System.out.println();
-
+                stringBuilder.append("  type=" + child.getGuiTypeAsString()+"\n");
 
             } else {
-
-
-                System.out.print(indryk + "'" + child.getLabel() + "'");
+                stringBuilder.append(indryk + "'" + child.getLabel() + "'");
                 if (child.getProperty().isMany()) {
                     if (child.getAddable()) {
-                        System.out.print(" (+)");
+                        stringBuilder.append(" (+)");
                     }
                     if (child.getRemovable()) {
-                        System.out.print("(-)");
+                        stringBuilder.append("(-)");
                     }
                     if (child.hasNonEmptyDescendant()) {
-                        System.out.print(" (d) ");
+                        stringBuilder.append(" (d) ");
                     }
                 }
-                System.out.println();
-
-                parseTree(child, indryk + "    ");
+                stringBuilder.append("\n");
+                parseTree(child, indryk + "    ", stringBuilder);
             }
+        }
+    }
+
+    public static String getStringFromFileOnClasspath(String filename) {
+        try {
+            return Strings.flush(Thread.currentThread().getContextClassLoader().getResourceAsStream(filename));
+        } catch (IOException e) {
+            fail(e.getMessage());
+            return null;
         }
     }
 }
