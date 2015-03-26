2b67c3470f0d2efeb343be533664e87afc11c92d
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-03-26 09:27:44 +0100

    Unit test for datastreams instead of online test

--- domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/DatastreamTest.java
+++ domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/DatastreamTest.java
@@ -1,75 +1,184 @@
 package dk.statsbiblioteket.doms.client.objects;
 
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Before;
+import org.junit.Test;
+
+import dk.statsbiblioteket.doms.central.CentralWebservice;
+import dk.statsbiblioteket.doms.central.Checksum;
+import dk.statsbiblioteket.doms.central.DatastreamProfile;
+import dk.statsbiblioteket.doms.central.ObjectProfile;
 import dk.statsbiblioteket.doms.client.datastreams.Datastream;
 import dk.statsbiblioteket.doms.client.datastreams.DatastreamDeclaration;
 import dk.statsbiblioteket.doms.client.datastreams.DatastreamModel;
-import dk.statsbiblioteket.doms.client.exceptions.NotFoundException;
-import dk.statsbiblioteket.doms.client.exceptions.ServerOperationFailed;
-import dk.statsbiblioteket.doms.client.exceptions.XMLParseException;
-import dk.statsbiblioteket.doms.client.relations.RelationDeclaration;
-import dk.statsbiblioteket.doms.client.relations.RelationModel;
+import dk.statsbiblioteket.doms.client.impl.objects.DigitalObjectFactoryImpl;
 import dk.statsbiblioteket.doms.client.sdo.SDOParsedXmlDocument;
 import dk.statsbiblioteket.doms.client.sdo.SDOParsedXmlElement;
 import dk.statsbiblioteket.doms.client.utils.Constants;
-import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.Ignore;
-import org.junit.Test;
 
-import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNotSame;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 /**
- * Created by IntelliJ IDEA.
- * User: abr
- * Date: 10/28/11
- * Time: 4:14 PM
- * To change this template use File | Settings | File Templates.
+ * Test working with datastreams.
  */
-public class DatastreamTest extends TestBase {
+public class DatastreamTest {
 
-    public DatastreamTest() throws MalformedURLException {
-        super();
+    private CentralWebservice centralWebservice;
+    private DigitalObjectFactory factory;
+
+    public DatastreamTest() {
     }
 
+    @Before
+    public void setUp() throws Exception {
+        centralWebservice = mock(CentralWebservice.class);
+        when(centralWebservice.getObjectProfile("doms:ContentModel_Program"))
+                .thenReturn(createContentModelProgramObjectProfile());
+        when(centralWebservice.getObjectProfile("doms:Template_Program"))
+                .thenReturn(createTemplateProgramObjectProfile());
 
+        when(centralWebservice.getDatastreamContents("doms:ContentModel_Program", "DS-COMPOSITE-MODEL"))
+                .thenReturn(createDSCompositeModel());
+        when(centralWebservice.getDatastreamContents("doms:ContentModel_Program", "ACCESS_SCHEMA"))
+                .thenReturn(createAccessSchema());
+        when(centralWebservice.getDatastreamContents("doms:Template_Program", "ACCESS"))
+                .thenReturn(createAccess());
+
+        factory = new DigitalObjectFactoryImpl(centralWebservice);
+    }
+
+    /**
+     * Test getting the datastream model.
+     */
     @Test
     public void testDatastreamModel() throws Exception {
+        //Call method
         ContentModelObject cmProgram = (ContentModelObject) factory.getDigitalObject("doms:ContentModel_Program");
-        assertTrue(cmProgram instanceof ContentModelObject);
-        if (cmProgram instanceof ContentModelObject) {
-            DatastreamModel dsModel = cmProgram.getDsModel();
-            assertTrue(dsModel.getDatastreamDeclarations().size() > 0);
-            assertNotNull(dsModel.getMimeType());
-            assertNotNull(dsModel.getFormatURI());
-            DatastreamDeclaration dsDcl = dsModel.getDatastreamDeclarations().get(0);
-        }
+        DatastreamModel dsModel = cmProgram.getDsModel();
+        List<DatastreamDeclaration> datastreamDeclarations = dsModel.getDatastreamDeclarations();
+
+        //Assert calls
+        //Should read the content model profile
+        verify(centralWebservice).getObjectProfile("doms:ContentModel_Program");
+        //Should read the content model's datastream model
+        verify(centralWebservice).getDatastreamContents("doms:ContentModel_Program", "DS-COMPOSITE-MODEL");
+        verifyNoMoreInteractions(centralWebservice);
+
+        assertEquals("Should get right mime type", "text/xml", dsModel.getMimeType());
+        assertNotNull("Should get right format URI", dsModel.getFormatURI());
+        assertEquals("Should find 1 declared datastream", 1, datastreamDeclarations.size());
+        DatastreamDeclaration dsDcl = datastreamDeclarations.get(0);
+        assertEquals("Should find the right declared datastream", "ACCESS", dsDcl.getName());
     }
 
+    /**
+     * Test getting reading a datastream.
+     */
     @Test
-    @Ignore("Alhena test disabled due to alhena not using java 8")
-    public void testDatastreamModel2() throws Exception {
+    public void testReadDatastreamDefinition() throws Exception {
+        //Call method
         DigitalObject template = factory.getDigitalObject("doms:Template_Program");
-        Set<DatastreamDeclaration> declarations = template.getDatastream("PBCORE").getDeclarations();
-        assertTrue(declarations.size() > 0);
-        for (DatastreamDeclaration declaration : declarations) {
-            assertEquals(declaration.getName(), "PBCORE");
-            assertTrue(declaration.getPresentation() == Constants.GuiRepresentation.editable);
-            Datastream pbcoreSchema = declaration.getSchema();
-            if (pbcoreSchema != null) {
-                assertNotNull(pbcoreSchema.getContents());
-            } else {
-                fail();
-            }
-        }
+        Set<DatastreamDeclaration> declarations = template.getDatastream("ACCESS").getDeclarations();
+
+        //Assert calls
+        //Should read the object profile
+        verify(centralWebservice).getObjectProfile("doms:Template_Program");
+        //Should read the object's content model's profile
+        verify(centralWebservice).getObjectProfile("doms:ContentModel_Program");
+        //Should read the object's content model's datastream model
+        verify(centralWebservice).getDatastreamContents("doms:ContentModel_Program", "DS-COMPOSITE-MODEL");
+        verifyNoMoreInteractions(centralWebservice);
+
+        //Check result
+        assertEquals("Should get the one declared datastream", 1, declarations.size());
+        DatastreamDeclaration declaration = declarations.iterator().next();
+        assertEquals(declaration.getName(), "ACCESS");
+        assertTrue(declaration.getPresentation() == Constants.GuiRepresentation.editable);
+
+        //Call method
+        Datastream accessSchema = declaration.getSchema();
+        String contents = accessSchema.getContents();
+
+        //Assert calls
+        //Should read the schema for the datastream
+        verify(centralWebservice).getDatastreamContents("doms:ContentModel_Program", "ACCESS_SCHEMA");
+        verifyNoMoreInteractions(centralWebservice);
+
+        //Check result
+        assertNotNull(contents);
     }
 
+    /**
+     * Test saving changes to a datastream.
+     */
+    @Test
+    public void testSaveDatastream() throws Exception {
+        //Call method
+        String destroyed = "doms:Template_Program";
+        DigitalObject object = factory.getDigitalObject(destroyed);
+        Datastream datastream = object.getDatastream("ACCESS");
+        SDOParsedXmlDocument doc = datastream.getSDOParsedDocument();
+
+        //Verify expected calls
+        //Should read the object profile
+        verify(centralWebservice).getObjectProfile(destroyed);
+        //Should read the object's content model's profile
+        verify(centralWebservice).getObjectProfile("doms:ContentModel_Program");
+        //Should read the object's content model's datastream model
+        verify(centralWebservice).getDatastreamContents("doms:ContentModel_Program", "DS-COMPOSITE-MODEL");
+        //Should read the object's content model's schema
+        verify(centralWebservice).getDatastreamContents("doms:ContentModel_Program", "ACCESS_SCHEMA");
+        //Should read the object's datastream
+        verify(centralWebservice).getDatastreamContents(destroyed, "ACCESS");
+        verifyNoMoreInteractions(centralWebservice);
+
+        //Set the object inactive
+        object.setState(Constants.FedoraState.Inactive);
+
+        //Make a change to the document
+        String originaldoc = doc.dumpToString();
+        changeField(doc.getRootSDOParsedXmlElement(), "test", "testvalue");
+        String unchangeddoc = doc.dumpToString();
+        assertTrue(XMLUnit.compareXML(originaldoc, unchangeddoc).identical());
+        changeField(doc.getRootSDOParsedXmlElement(), "Defekt", "test of change: " + Math.random());
+        String changeddoc = doc.dumpToString();
+        assertFalse(XMLUnit.compareXML(originaldoc, changeddoc).identical());
+
+        //Save the document
+        object.save();
+
+        //Verify expected interactions
+        //Set object inactive
+        verify(centralWebservice).markInProgressObject(eq(Arrays.asList(destroyed)), anyString());
+        //Modify datastream
+        verify(centralWebservice).modifyDatastream(eq(destroyed), eq("ACCESS"), eq(changeddoc), anyString());
+        verifyNoMoreInteractions(centralWebservice);
+
+        //Try getting the datastream
+        DigitalObject object2 = factory.getDigitalObject(destroyed);
+        SDOParsedXmlDocument doc2 = object2.getDatastream("ACCESS").getSDOParsedDocument();
+        String rereaddoc = doc2.dumpToString();
+
+        //Should not reread it from DOMS
+        verifyNoMoreInteractions(centralWebservice);
+
+        //Should get the updated document
+        assertTrue(XMLUnit.compareXML(changeddoc, rereaddoc).identical());
+    }
 
     private void changeField(SDOParsedXmlElement doc, String field, String newvalue) {
         ArrayList<SDOParsedXmlElement> children = doc.getChildren();
@@ -84,121 +193,93 @@ public class DatastreamTest extends TestBase {
         }
     }
 
+    private ObjectProfile createContentModelProgramObjectProfile() {
+        ObjectProfile profile = new ObjectProfile();
+        profile.setPid("doms:ContentModel_Program");
+        profile.setState("A");
+        profile.setType("ContentModel");
 
-    @Test
-    @Ignore("Alhena test disabled due to alhena not using java 8")
-    public void testSaveDatastream() throws Exception {
+        DatastreamProfile datastreamProfile = new DatastreamProfile();
+        datastreamProfile.setId("DS-COMPOSITE-MODEL");
+        Checksum checksum = new Checksum();
+        checksum.setType("DISABLED");
+        datastreamProfile.setChecksum(checksum);
+        datastreamProfile.setMimeType("text/xml");
+        datastreamProfile.setFormatUri("info:fedora/fedora-system:FedoraDSCompositeModel-1.0");
+        profile.getDatastreams().add(datastreamProfile);
 
+        DatastreamProfile datastreamProfile2 = new DatastreamProfile();
+        datastreamProfile2.setId("ACCESS_SCHEMA");
+        Checksum checksum2 = new Checksum();
+        checksum2.setType("DISABLED");
+        datastreamProfile2.setChecksum(checksum2);
+        profile.getDatastreams().add(datastreamProfile2);
 
-        String destroyed = victimProgram;
-        DigitalObject object = factory.getDigitalObject(destroyed);
-        Datastream datastream = object.getDatastream("PBCORE");
-        SDOParsedXmlDocument doc = datastream.getSDOParsedDocument();
-        object.setState(Constants.FedoraState.Inactive);
-        object.save();
-        String originaldoc = doc.dumpToString();
-
-        changeField(doc.getRootSDOParsedXmlElement(), "test", "testvalue");
-
-
-        String unchangeddoc = doc.dumpToString();
-
-        assertTrue(XMLUnit.compareXML(originaldoc, unchangeddoc).identical());
-
-        changeField(doc.getRootSDOParsedXmlElement(), "Subject", "test of change: " + Math.random());
-
-
-        String changeddoc = doc.dumpToString();
-
-        assertNotSame(originaldoc, unchangeddoc);
-
-
-/*
-        if (datastream instanceof InternalDatastream) {
-            InternalDatastream internalDatastream = (InternalDatastream) datastream;
-            internalDatastream.replace(doc.dumpToString());
-        }
-*/
-        object.save();
-
-        setUp();
-        DigitalObject object2 = factory.getDigitalObject(destroyed);
-        SDOParsedXmlDocument doc2 = object2.getDatastream("PBCORE").getSDOParsedDocument();
-        String rereaddoc = doc2.dumpToString();
-
-
-        //TODO make this comparison work, they are xml alike
-        assertTrue(XMLUnit.compareXML(changeddoc, rereaddoc).identical());
-
-
+        return profile;
     }
 
-    @Test
-    @Ignore("Alhena test disabled due to alhena not using java 8")
-    public void testXmlParseExceptions() throws ServerOperationFailed, XMLParseException, NotFoundException {
-        boolean createdProgram = false;
-        boolean createdShard = false;
-        boolean createdRelation = false;
-        boolean saved = false;
+    private ObjectProfile createTemplateProgramObjectProfile() {
+        ObjectProfile profile = new ObjectProfile();
+        profile.setPid("doms:Template_Program");
+        profile.setState("A");
+        profile.setTitle("");
+        profile.setType("TemplateObject");
 
-        DigitalObject newProgram = null;
-        DigitalObject shard = null;
+        profile.getContentmodels().add("doms:ContentModel_Program");
 
-        DigitalObject object = factory.getDigitalObject("doms:RadioTV_Collection");
-        if (object instanceof CollectionObject) {
-            CollectionObject collectionObject = (CollectionObject) object;
-            Set<TemplateObject> entryTemplates = collectionObject.getEntryTemplates("GUI");
-            for (TemplateObject entryTemplate : entryTemplates) {
-                if (entryTemplate.getPid().equals("doms:Template_Program")) {
+        DatastreamProfile datastreamProfile = new DatastreamProfile();
+        datastreamProfile.setId("ACCESS");
+        Checksum checksum = new Checksum();
+        checksum.setType("DISABLED");
+        datastreamProfile.setChecksum(checksum);
+        datastreamProfile.setInternal(true);
+        profile.getDatastreams().add(datastreamProfile);
 
-                    newProgram = entryTemplate.clone();
-                    createdProgram = true;
-                    for (ContentModelObject contentModelObject : newProgram.getType()) {
-                        RelationModel relModel = contentModelObject.getRelationModel();
-                        for (RelationDeclaration relationDeclaration : relModel.getRelationDeclarations()) {
-                            if (relationDeclaration.getViewAngles().contains("GUI")) {
-                                Set<ContentModelObject> firstLevelObjects = relationDeclaration.getFirstLevelModels();
-                                for (ContentModelObject firstLevelObject : firstLevelObjects) {
-                                    Set<TemplateObject> templateDeep = firstLevelObject.getTemplates();
-                                    if (templateDeep.size() > 0) {
-
-                                        shard = templateDeep.iterator().next().clone();
-                                        createdShard = true;
-                                        newProgram.addObjectRelation(relationDeclaration.getPredicate(), shard);
-                                        createdRelation = true;
-                                        newProgram.save("GUI");
-                                        saved = true;
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                    }
-
-                }
-            }
-        } else {
-
-            fail();
-        }
-        if (newProgram != null) {
-            newProgram.setState(Constants.FedoraState.Deleted);
-            newProgram.save();
-        }
-        if (shard != null) {
-
-            //TODO test was according to old datamodel
-            /*parseDoc(shard.getDatastream("SHARD_METADATA").getSDOParsedDocument());
-            emptymize(shard.getDatastream("SHARD_METADATA").getSDOParsedDocument().getRootSDOParsedXmlElement());
-            parseDoc(shard.getDatastream("SHARD_METADATA").getSDOParsedDocument());
-            shard.setState(Constants.FedoraState.Deleted);
-            try {
-                shard.save();
-                fail();
-            } catch (XMLParseException e){
-
-            }*/
-        }
+        return profile;
     }
 
+    private String createDSCompositeModel() {
+        return "<dsCompositeModel\n"
+                + "        xmlns=\"info:fedora/fedora-system:def/dsCompositeModel#\">\n"
+                + "    <dsTypeModel ID=\"ACCESS\">\n"
+                + "        <form MIME=\"text/xml\"/>\n"
+                + "        <extension name=\"GUI\">\n"
+                + "            <presentAs type=\"editable\"/>\n"
+                + "        </extension>\n"
+                + "        <extension name=\"SCHEMA\">\n"
+                + "            <reference type=\"datastream\" value=\"ACCESS_SCHEMA\"/>\n"
+                + "        </extension>\n"
+                + "    </dsTypeModel>\n"
+                + "</dsCompositeModel>\n"
+                + "\n";
+    }
+
+    private String createAccessSchema() {
+        return "<xs:schema xmlns:tns=\"http://doms.statsbiblioteket.dk/types/access/0/1/#\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\"\n"
+                + "  version=\"1.0\" targetNamespace=\"http://doms.statsbiblioteket.dk/types/access/0/1/#\"\n"
+                + "  attributeFormDefault=\"unqualified\" elementFormDefault=\"qualified\">\n"
+                + "  <xs:element name=\"access\" type=\"tns:access\"/>\n" 
+                + "\n"
+                + "  <xs:complexType name=\"access\">\n" 
+                + "    <xs:sequence>\n"
+                + "      <xs:element name=\"individuelt_forbud\" type=\"tns:yesNoType\" minOccurs=\"0\" maxOccurs=\"1\" />\n"
+                + "      <xs:element name=\"klausuleret\" type=\"tns:yesNoType\" minOccurs=\"0\" maxOccurs=\"1\" />\n"
+                + "      <xs:element name=\"defekt\" type=\"tns:yesNoType\" minOccurs=\"0\" maxOccurs=\"1\" />\n"
+                + "      <xs:element name=\"kommentarer\" type=\"xs:string\" minOccurs=\"0\" maxOccurs=\"1\"/>\n"
+                + "    </xs:sequence>\n" 
+                + "  </xs:complexType>\n"
+                + "\n"
+                + "  <xs:simpleType name=\"yesNoType\">\n"
+                + "      <xs:restriction base=\"xs:string\">\n" 
+                + "          <xs:enumeration value=\"Nej\"/>\n"
+                + "          <xs:enumeration value=\"Ja\"/>\n" 
+                + "      </xs:restriction>\n"
+                + "  </xs:simpleType>\n"
+                + "</xs:schema>\n";
+    }
+
+    private String createAccess() {
+        return "<access xmlns=\"http://doms.statsbiblioteket.dk/types/access/0/1/#\">"
+                + "</access>";
+    }
 }
