1b47e8a4d6ac19769098b118c474c8d8c81384be
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-04-16 14:02:30 +0200

    Offline relation tests

--- domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/RelationsTest.java
+++ domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/RelationsTest.java
@@ -1,144 +1,330 @@
 package dk.statsbiblioteket.doms.client.objects;
 
+import org.hamcrest.Description;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+
+import dk.statsbiblioteket.doms.central.CentralWebservice;
+import dk.statsbiblioteket.doms.central.Checksum;
+import dk.statsbiblioteket.doms.central.DatastreamProfile;
+import dk.statsbiblioteket.doms.central.ObjectProfile;
+import dk.statsbiblioteket.doms.client.impl.objects.DigitalObjectFactoryImpl;
 import dk.statsbiblioteket.doms.client.relations.LiteralRelation;
 import dk.statsbiblioteket.doms.client.relations.ObjectRelation;
 import dk.statsbiblioteket.doms.client.relations.Relation;
-import dk.statsbiblioteket.doms.client.utils.Constants;
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
 
 import java.net.MalformedURLException;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 /**
- * Created by IntelliJ IDEA.
- * User: abr
- * Date: 10/28/11
- * Time: 4:14 PM
- * To change this template use File | Settings | File Templates.
+ * Test relation operations
  */
-public class RelationsTest extends TestBase {
+public class RelationsTest {
+
+    private static final String UUID = "uuid:XXX";
+    private static final String CM_PID = "doms:ContentModel_Program";
+    private DigitalObjectFactory factory;
+    private CentralWebservice domsAPI;
 
     public RelationsTest() throws MalformedURLException {
         super();
     }
 
-    @Test
-    public void testViewRelations() throws Exception {
-        DigitalObject cmdoms = factory.getDigitalObject("doms:ContentModel_Program");
-        assertTrue(cmdoms instanceof ContentModelObject);
-        if (cmdoms instanceof ContentModelObject) {
-            ContentModelObject cmo = (ContentModelObject) cmdoms;
-            assertNotNull(cmo.getRelationsWithViewAngle("SummaVisible"));
-        }
+    @Before
+    public void setUp() throws Exception {
+        domsAPI = mock(CentralWebservice.class);
+        factory = new DigitalObjectFactoryImpl(domsAPI);
+        when(domsAPI.getObjectProfile(UUID)).thenReturn(createObjectProfile());
+        when(domsAPI.getObjectProfile(CM_PID)).thenReturn(createCMObjectProfile());
+        when(domsAPI.getDatastreamContents(CM_PID, "VIEW")).thenReturn(createView());
     }
 
+    /**
+     * Test that relations are related to their content model view annotations.
+     *
+     * @throws Exception
+     */
     @Test
-    @Ignore //Will run for ever
+    public void testViewRelations() throws Exception {
+        //Read content model object
+        DigitalObject cmdoms = factory.getDigitalObject(CM_PID);
+
+        //This should call DOMS
+        verify(domsAPI).getObjectProfile(CM_PID);
+        verifyNoMoreInteractions(domsAPI);
+
+        //Read view relations
+        ContentModelObject cmo = (ContentModelObject) cmdoms;
+        List<String> relationsWithViewAngle = cmo.getRelationsWithViewAngle("SummaVisible");
+
+        //This should trigger a read of the VIEW relations
+        verify(domsAPI).getDatastreamContents(CM_PID, "VIEW");
+        verifyNoMoreInteractions(domsAPI);
+
+        //A view relation should be found.
+        assertNotNull(relationsWithViewAngle);
+        assertEquals(1, relationsWithViewAngle.size());
+        assertEquals("http://doms.statsbiblioteket.dk/relations/default/0/1/#hasFile", relationsWithViewAngle.get(0));
+    }
+
+    /**
+     * Test getting inverse relations.
+     * @throws Exception
+     */
+    @Test
     public void testInverseRelations() throws Exception {
-        DigitalObject cmdoms = factory.getDigitalObject("doms:ContentModel_Program");
-        assertTrue(cmdoms instanceof ContentModelObject);
+        //Read content model object
+        DigitalObject cmdoms = factory.getDigitalObject(CM_PID);
+
+        //This should call DOMS
+        verify(domsAPI).getObjectProfile(CM_PID);
+        verifyNoMoreInteractions(domsAPI);
+
+        //Read inverse relations
         List<ObjectRelation> inverseRelations = cmdoms.getInverseRelations();
+
+        //This should call DOMS
+        verify(domsAPI).getInverseRelations(CM_PID);
+        verifyNoMoreInteractions(domsAPI);
+
+        //Check results
         for (ObjectRelation inverseRelation : inverseRelations) {
             assertEquals(inverseRelation.getObjectPid(), cmdoms.getPid());
             assertNotNull(inverseRelation.getSubject());
         }
+        verifyNoMoreInteractions(domsAPI);
     }
 
+    /**
+     * Test adding literal relations.
+     * @throws Exception
+     */
     @Test
     public void testAddedRelationLiteral() throws Exception {
-        DigitalObject object = factory.getDigitalObject(victimProgram);
+        //Read object
+        DigitalObject object = factory.getDigitalObject(UUID);
 
-        object.setState(Constants.FedoraState.Inactive); //To open it up for changes
+        //This should call DOMS
+        verify(domsAPI).getObjectProfile(UUID);
+        verifyNoMoreInteractions(domsAPI);
+
+        //Add a relation
+        LiteralRelation newRel = object
+                .addLiteralRelation("http://domclient.unittests/#testRelationPredicateLiteral", "literalValue");
+
+        //This should not call DOMS until the object is saved.
+        verifyNoMoreInteractions(domsAPI);
+
+        //Save the change
         object.save();
 
-        LiteralRelation newRel = object.addLiteralRelation(
-                "http://domclient.unittests/#testRelationPredicateLiteral", "literalValue");
-        object.save();
+        //This should call DOMS
+        verify(domsAPI).addRelation(eq(UUID),
+                                    isThisRelation(UUID, "http://domclient.unittests/#testRelationPredicateLiteral",
+                                                   "literalValue", true), anyString());
+        verifyNoMoreInteractions(domsAPI);
+    }
 
-        setUp();
-        object = factory.getDigitalObject(victimProgram);
-        boolean present = false;
+    /**
+     * Test removing literal relations.
+     * @throws Exception
+     */
+    @Test
+    public void testRemovedRelationLiteral() throws Exception {
+        //Read object
+        DigitalObject object = factory.getDigitalObject(UUID);
+
+        //This should call DOMS
+        verify(domsAPI).getObjectProfile(UUID);
+        verifyNoMoreInteractions(domsAPI);
+
+        //Find literal relation
+        LiteralRelation literalRelation = null;
         for (Relation relation : object.getRelations()) {
-            if (relation.equals(newRel)) {
-                present = true;
-                newRel = (LiteralRelation) relation;
+            if (relation instanceof LiteralRelation) {
+                literalRelation = (LiteralRelation) relation;
                 break;
             }
         }
-        Assert.assertTrue(present);
-        newRel.remove();
-        present = false;
-        for (Relation relation : object.getRelations()) {
-            if (relation.equals(newRel)) {
-                present = true;
-                fail("Relation should have been removed");
-            }
-        }
+        assertNotNull(literalRelation);
+
+        //Remove literal relation
+        literalRelation.remove();
+
+        //This should not call DOMS until the object is saved.
+        verifyNoMoreInteractions(domsAPI);
+
+        //Save the change
         object.save();
 
-        setUp();
-        object = factory.getDigitalObject(victimProgram);
-        for (Relation relation : object.getRelations()) {
-            if (relation.equals(newRel)) {
-                present = true;
-                fail("Relation should have been removed");
-            }
-        }
-        object.setState(Constants.FedoraState.Active); //To open it up for changes
-        object.save();
-
-
+        //This should call DOMS
+        verify(domsAPI).deleteRelation(eq(UUID),
+                                       isThisRelation(literalRelation.getSubjectPid(), literalRelation.getPredicate(),
+                                                      literalRelation.getObject(), true), anyString());
+        verifyNoMoreInteractions(domsAPI);
     }
 
+    /**
+     * Test adding object relations.
+     * @throws Exception
+     */
     @Test
     public void testAddedRelationObject() throws Exception {
-        DigitalObject object = factory.getDigitalObject(victimProgram);
+        //Read object
+        DigitalObject object = factory.getDigitalObject(UUID);
 
-        object.setState(Constants.FedoraState.Inactive); //To open it up for changes
+        //This should call DOMS
+        verify(domsAPI).getObjectProfile(UUID);
+        verifyNoMoreInteractions(domsAPI);
+
+        //Add a relation
+        object.addObjectRelation("http://domclient.unittests/#testRelationPredicateObject",
+                                                   object);
+
+        //This should not call DOMS until the object is saved.
+        verifyNoMoreInteractions(domsAPI);
+
+        //Save the change
         object.save();
 
-        Relation newRel = object.addObjectRelation(
-                "http://domclient.unittests/#testRelationPredicate", object.getType().get(0));
-        object.save();
+        //This should call DOMS
+        verify(domsAPI).addRelation(eq(UUID),
+                                    isThisRelation(UUID, "http://domclient.unittests/#testRelationPredicateObject",
+                                                   UUID, false), anyString());
+        verifyNoMoreInteractions(domsAPI);
+    }
 
-        setUp();
-        object = factory.getDigitalObject(victimProgram);
-        boolean present = false;
+    /**
+     * Test removing object relations.
+     * @throws Exception
+     */
+    @Test
+    public void testRemovedRelationObject() throws Exception {
+        //Read object
+        DigitalObject object = factory.getDigitalObject(UUID);
+
+        //This should call DOMS
+        verify(domsAPI).getObjectProfile(UUID);
+        verifyNoMoreInteractions(domsAPI);
+
+        //Find object relation
+        ObjectRelation objectRelation = null;
         for (Relation relation : object.getRelations()) {
-            if (relation.equals(newRel)) {
-                present = true;
-                newRel = relation;
+            if (relation instanceof ObjectRelation) {
+                objectRelation = (ObjectRelation) relation;
                 break;
             }
         }
-        Assert.assertTrue(present);
-        newRel.remove();
-        present = false;
-        for (Relation relation : object.getRelations()) {
-            if (relation.equals(newRel)) {
-                present = true;
-                fail("Relation should have been removed");
-            }
-        }
+        assertNotNull(objectRelation);
+
+        //Remove object relation
+        objectRelation.remove();
+
+        //This should not call DOMS until the object is saved.
+        verifyNoMoreInteractions(domsAPI);
+
+        //Save the change
         object.save();
 
-        setUp();
-        object = factory.getDigitalObject(victimProgram);
-        for (Relation relation : object.getRelations()) {
-            if (relation.equals(newRel)) {
-                present = true;
-                fail("Relation should have been removed");
-            }
-        }
-
+        //This should call DOMS
+        verify(domsAPI).deleteRelation(eq(UUID),
+                                       isThisRelation(objectRelation.getSubjectPid(), objectRelation.getPredicate(),
+                                                      objectRelation.getObjectPid(), false), anyString());
+        verifyNoMoreInteractions(domsAPI);
 
     }
 
+    private ObjectProfile createObjectProfile() {
+        ObjectProfile objectProfile = new ObjectProfile();
+        objectProfile.setPid(UUID);
+        objectProfile.setState("I");
+        objectProfile.setTitle("Object");
+        dk.statsbiblioteket.doms.central.Relation relation1 = new dk.statsbiblioteket.doms.central.Relation();
+        relation1.setSubject("info:fedora/" + UUID);
+        relation1.setPredicate("info:test/relation");
+        relation1.setObject("Hello");
+        relation1.setLiteral(true);
+        dk.statsbiblioteket.doms.central.Relation relation2 = new dk.statsbiblioteket.doms.central.Relation();
+        relation2.setSubject("info:fedora/" + UUID);
+        relation2.setPredicate("info:test/relation2");
+        relation2.setObject("info:fedora/doms:ContentModel_Program");
+        relation2.setLiteral(false);
+        objectProfile.getRelations().add(relation1);
+        objectProfile.getRelations().add(relation2);
+        return objectProfile;
+    }
+
+    private ObjectProfile createCMObjectProfile() {
+        ObjectProfile objectProfile = new ObjectProfile();
+        objectProfile.setPid(CM_PID);
+        objectProfile.setType("ContentModel");
+        objectProfile.setState("A");
+        DatastreamProfile datastreamProfile = new DatastreamProfile();
+        datastreamProfile.setId("VIEW");
+        Checksum checksum = new Checksum();
+        checksum.setType("DISABLED");
+        datastreamProfile.setChecksum(checksum);
+        objectProfile.getDatastreams().add(datastreamProfile);
+        return objectProfile;
+    }
+
+    private String createView() {
+        return "<v:views xmlns:v=\"http://doms.statsbiblioteket.dk/types/view/default/0/1/#\">\n"
+                + "    <v:viewangle name=\"SummaVisible\">\n"
+                + "        <v:relations xmlns:doms=\"http://doms.statsbiblioteket.dk/relations/default/0/1/#\">\n"
+                + "            <doms:hasFile/>\n"
+                + "        </v:relations>\n"
+                + "        <v:inverseRelations/>\n"
+                + "    </v:viewangle>\n"
+                + "</v:views>\n";
+    }
+
+
+    private static dk.statsbiblioteket.doms.central.Relation isThisRelation(String subject, String predicate, String object, boolean literal) {
+        return argThat(new IsThisRelation(subject, predicate, object, literal));
+    }
+
+    private static class IsThisRelation extends ArgumentMatcher<dk.statsbiblioteket.doms.central.Relation> {
+        private final String subject;
+        private final String predicate;
+        private final String object;
+        private final boolean literal;
+
+        public IsThisRelation(String subject, String predicate, String object, boolean literal) {
+            this.subject = subject;
+            this.predicate = predicate;
+            this.object = object;
+            this.literal = literal;
+        }
+
+        @Override
+        public boolean matches(Object argument) {
+            dk.statsbiblioteket.doms.central.Relation that = (dk.statsbiblioteket.doms.central.Relation) argument;
+            return (clean(subject).equals(clean(that.getSubject()))
+                    && predicate.equals(that.getPredicate())
+                    && clean(object).equals(clean(that.getObject()))
+                    && literal == that.isLiteral());
+        }
+
+        private String clean(String s) {
+            return s.replaceAll("^info:fedora/", "");
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendValue(
+                    String.format("<relation: (%s, %s, %s), literal: %s>", subject, predicate, object, literal));
+        }
+    }
 }
