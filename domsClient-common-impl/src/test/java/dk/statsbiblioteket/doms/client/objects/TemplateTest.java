6e386072cf730e5f5c53fba216560e8a2bf58b66
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-04-16 14:22:08 +0200

    Offline template tests

--- domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/TemplateTest.java
+++ domsClient-common-impl/src/test/java/dk/statsbiblioteket/doms/client/objects/TemplateTest.java
@@ -1,43 +1,87 @@
 package dk.statsbiblioteket.doms.client.objects;
 
 
-import dk.statsbiblioteket.doms.client.exceptions.ServerOperationFailed;
-import dk.statsbiblioteket.doms.client.utils.Constants;
-import org.junit.Ignore;
+import dk.statsbiblioteket.doms.central.CentralWebservice;
+import dk.statsbiblioteket.doms.central.ObjectProfile;
+import dk.statsbiblioteket.doms.client.impl.objects.DigitalObjectFactoryImpl;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 /**
- * Created by IntelliJ IDEA.
- * User: abr
- * Date: 10/31/11
- * Time: 11:10 AM
- * To change this template use File | Settings | File Templates.
+ * Test reading templates.
  */
-public class TemplateTest extends TestBase {
+public class TemplateTest {
+    private DigitalObjectFactoryImpl factory;
+    private CentralWebservice domsAPI;
 
     public TemplateTest() throws MalformedURLException {
         super();
     }
 
-    @Test
-    public void testTemplateCreation() throws ServerOperationFailed {
-        DigitalObject template = factory.getDigitalObject("doms:Template_Program");
-        if (template instanceof TemplateObject) {
-            TemplateObject template1 = (TemplateObject) template;
-            DigitalObject newObject = template1.clone();
-            assertTrue(newObject.getPid().startsWith("uuid:"));
-            assertEquals(newObject.getState(), Constants.FedoraState.Inactive);
-            assertTrue(newObject instanceof DataObject);
-            newObject.setState(Constants.FedoraState.Deleted);
-        } else {
-            fail("Template is not template");
-        }
+    @Before
+    public void setUp() throws Exception {
+        domsAPI = mock(CentralWebservice.class);
+        when(domsAPI.getObjectProfile("doms:Template_Program")).thenReturn(createTemplateObjectProfile());
+        when(domsAPI.getObjectProfile("uuid:XXX")).thenReturn(createNewObjectProfile());
+        when(domsAPI.newObject(eq("doms:Template_Program"), eq(Collections.<String>emptyList()), anyString())).thenReturn(
+                "uuid:XXX");
+        factory = new DigitalObjectFactoryImpl(domsAPI);
+    }
 
+    /**
+     * Test cloning a template.
+     * @throws Exception
+     */
+    @Test
+    public void testTemplateCreation() throws Exception {
+        //Read template from doms.
+        DigitalObject template = factory.getDigitalObject("doms:Template_Program");
+
+        //Verify doms is called.
+        verify(domsAPI).getObjectProfile("doms:Template_Program");
+        verifyNoMoreInteractions(domsAPI);
+
+        //Clone the template
+        TemplateObject template1 = (TemplateObject) template;
+        DigitalObject clone = template1.clone();
+
+        //Verify that doms is called to clone from template, and reads the new object
+        verify(domsAPI).newObject(eq("doms:Template_Program"), eq(Collections.<String>emptyList()), anyString());
+        verify(domsAPI).getObjectProfile("uuid:XXX");
+        verifyNoMoreInteractions(domsAPI);
+
+        //Check result
+        assertEquals("uuid:XXX", clone.getPid());
+    }
+
+    private ObjectProfile createTemplateObjectProfile() {
+        ObjectProfile profile = new ObjectProfile();
+        profile.setType("TemplateObject");
+        profile.setPid("doms:Template_Program");
+        profile.setState("A");
+        return profile;
+    }
+
+    private ObjectProfile createNewObjectProfile() {
+        ObjectProfile profile = new ObjectProfile();
+        profile.setType("TemplateObject");
+        profile.setPid("uuid:XXX");
+        profile.setState("I");
+        return profile;
     }
 }
